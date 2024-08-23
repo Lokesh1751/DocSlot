61a0aedabf321dd67c864ad9748596755a6f78a9
Lokesh Kumar <lokeshkumar@Lokeshs-MacBook-Pro.local>
2024-08-23 12:58:43 +0530

    add user icon

--- src/HomePageComponents/Navbar.jsx
+++ src/HomePageComponents/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { FIREBASE_AUTH, FIRESTORE_DB } from "../pages/firebase.config";
@@ -124,19 +124,24 @@ const Navbar = () => {
           <div className="flex gap-6 items-center">
             {!user ? (
               <Link to="/Login" className="flex justify-center">
-                <p className="text-lg font-bold text-[#0143BE] cursor-pointer transition-all">
+                <p className="text-lg font-bold text-[#0143BE] mt-3 cursor-pointer transition-all">
                   Login | SignUp
                 </p>
               </Link>
             ) : (
               <div className="flex items-center gap-4">
                 <div className="flex items-center relative">
-                  <div className="flex flex-col">
-                    <p
-                      className="text-lg font-bold bg-[#0E02F5] text-white rounded-full px-4 py-2 cursor-pointer transition-all"
+                  <div className="flex  items-center gap-4">
+                    <FaUser
+                      className="w-[40px] h-[40px] cursor-pointer  rounded-full"
+                      color="#0143BE"
                       onClick={() => setProfileVisible(!profileVisible)}
+                    />
+                    <p
+                      onClick={handleLogout}
+                      className="text-xl  mt-3 flex items-center font-bold text-[#0143BE] cursor-pointer transition-all"
                     >
-                      {user.email.slice(0, 1).toUpperCase()}
+                      Logout
                     </p>
                     {profileVisible && (
                       <Link
@@ -149,12 +154,6 @@ const Navbar = () => {
                     )}
                   </div>
                 </div>
-                <p
-                  onClick={handleLogout}
-                  className="text-lg font-bold text-[#0143BE] cursor-pointer transition-all"
-                >
-                  Logout
-                </p>
               </div>
             )}
           </div>
